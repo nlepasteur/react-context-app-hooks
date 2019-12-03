5eb32aeecd65ca9c182a826091b4b06da27aa99d
Nicolas Le Pasteur <nicolas.lepasteur@gmail.com>
2019-12-03 14:04:55 +0100

    first hook

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: 'Quando sono con te', id: 1 },
+    { title: 'Buon compleano', id: 2 },
+    { title: 'Felicita putana', id: 3 }
+  ])
+
+  const addSong = () => {
+    setSongs([...songs, { title: 'Fatte spuglia', id: 4 }])
+  }
+
   return (
     <div className='song-list'>
+      <button onClick={addSong}>Add a song</button>
       <ul>
-        <li>this wild darkness</li>
-        <li>memory gospel</li>
+        {songs.map(song => (
+          <li>{song.title}</li>
+        ))}
       </ul>
     </div>
   )
